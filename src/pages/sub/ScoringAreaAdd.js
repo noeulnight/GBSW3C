541ab6f5b498c96c6fe174c602e8c31182e1c7f9
gimmuil <90828017+gimmuil@users.noreply.github.com>
2022-07-18 01:56:18 +0900

    Co-authored-by: Park Min Hyeok <opensource@pmh.codes> Co-authored-by: 임태현 <admin@limtaehyun.dev>

--- src/pages/sub/ScoringAreaAdd.js
+++ src/pages/sub/ScoringAreaAdd.js
@@ -1,22 +1,24 @@
 import React from 'react'
-import styles from '../../css/ScoringAreaAdd'
+import styles from '../../css/ScoringAreaAdd.module.scss'
 
 const ScoringAreaAdd = () => {
   return (
     <div>
-      <div className={styles.first}>
-        <input />
-        <input />
-      </div>
-      <div className={styles.second}>
-        <input type='number' />
-        <select></select>
-      </div>
-      <div>
-        {/* 캘린더 */}
-      </div>
-      <div className={styles.btn}>
-        <button value='다음' />
+      <div >
+        <div className={styles.first}>
+          <input />
+          <input />
+        </div>
+        <div className={styles.second}>
+          <input type='number' />
+          <select></select>
+        </div>
+        <div>
+          {/* 캘린더 */}
+        </div>
+        <div className={styles.btn}>
+          <button value='다음' />
+        </div>
       </div>
     </div>
   )
